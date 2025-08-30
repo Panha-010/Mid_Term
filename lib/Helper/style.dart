5a33acba2b10611ee00748ff45d52d3cb7635541
Panha-010 <panhaprum12@gmail.com>
2025-08-30 10:32:06 +0700

    Add files via upload

---
+++ lib/Helper/style.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:hexcolor/hexcolor.dart';
+
+class Stylecolor {
+  static const Color primary = Color(0xFF2E7D32); // Forest Green
+  static const Color background = Color(
+    0xFFF1F8E9,
+  ); // Light greenish background
+  static const Color filledBackground = Color(0xFF66BB6A); // Fresh Green
+  static const Color filledBackground2 = Color(0x2266BB6A); // Transparent green
+}
